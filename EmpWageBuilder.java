5c2e06f3a87267cba1e9cf1e4d52a425acdf2d7b
mouna07 <mounamc261@gmail.com>
2021-02-05 15:15:46 +0530

    Added: intial commit

---
+++ EmpWageBuilder.java
@@ -0,0 +1,8 @@
+public class EmpWageBuilder {
+    public static void main(String[] args) throws Exception {
+
+        // Welcome message
+        System.out.println("Welcome to Employee Wedge Builder...");
+        System.out.println("************************************");
+	}
+}
