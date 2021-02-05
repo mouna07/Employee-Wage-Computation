3433e8fee69d03e1bda5c89af9f09dc760833194
mouna07 <mounamc261@gmail.com>
2021-02-05 15:55:20 +0530

    REFACTORED: save wage by companies

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,11 +2,24 @@ public class EmpWageBuilder {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
 
+    // Variables
+    private final String company;
+    private final int empRatePerHr;
+    private final int noOfWorkingDays;
+    private final int maxHrsPeronth;
+
+    // Constructer
+    public EmpWageBuilder(String company, int empRatePerHr, int noOfWorkingDays, int maxHrsPeronth) {
+        this.company = company;
+        this.empRatePerHr = empRatePerHr;
+        this.noOfWorkingDays = noOfWorkingDays;
+        this.maxHrsPeronth = maxHrsPeronth;
+    }// End of Constructer
+
     // Compute Wage method
-    public static int computeEmpWage(String company, int empRatePerHr, int noOfWorkingDays, int maxHrsPeronth) {
+    public void computeEmpWage() {
         // Variables
         int empHrs = 0;
-        int empWage = 0;
         int totalEmpWage = 0;
         int totalWorkingDays = 0;
         int totalEmpHrs = 0;
@@ -26,13 +39,13 @@ public class EmpWageBuilder {
                 default:
                     empHrs = 0;
                     break;
-            }
+            }// End Case
             totalEmpHrs += empHrs;
             System.out.println("Day: " + totalWorkingDays + "\tEmp Hr: " + empHrs);
-        }
+        } // End While
         totalEmpWage = totalEmpHrs * empRatePerHr;
         System.out.println("Total Employee Wage for company " + company + ": " + totalEmpWage + "\n");
-        return totalEmpWage;
+
     } // End computeEmpWage
 
     public static void main(String[] args) throws Exception {
@@ -40,8 +53,14 @@ public class EmpWageBuilder {
         // Welcome message
         System.out.println("Welcome to Employee Wedge Builder...");
         System.out.println("************************************\n");
-        computeEmpWage("Dmart", 20, 2, 10);
-        computeEmpWage("Reliance", 10, 4, 20);
-    }// end main
-}// end class EmpWageBuilder
 
+        // Objects
+        EmpWageBuilder dMart = new EmpWageBuilder("Dmart", 20, 2, 10);
+        EmpWageBuilder reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
+
+        // Method calls
+        dMart.computeEmpWage();
+        reliance.computeEmpWage();
+
+    }// End main
+}// End class EmpWageBuilder
