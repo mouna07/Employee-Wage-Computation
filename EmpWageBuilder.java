4aec1222ef45b413134ab4aa8fc1f2a719e9eab4
mouna07 <mounamc261@gmail.com>
2021-02-05 15:29:25 +0530

    REFACTORED: case statement

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,32 +1,34 @@
 public class EmpWageBuilder {
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HR = 20;
+
     public static void main(String[] args) throws Exception {
 
         // Welcome message
         System.out.println("Welcome to Employee Wedge Builder...");
         System.out.println("************************************");
 
-        // Constants
-	final int IS_PART_TIME = 1;
-        final int IS_FULL_TIME = 2;
-        final int EMP_RATE_PER_HR = 20;
-
         // Variables
         int empHrs = 0;
         int empWage = 0;
 
         // Computation
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        System.out.println(empCheck);
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         // Case Checking
-        if (empCheck == IS_PART_TIME) {
-            System.out.println("Employee is Present.");
-            empHrs = 4;
-        } else if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present.");
-            empHrs = 8;
-        } else {
-            System.out.println("Employee is Absent.");
-            empHrs = 0;
+        switch (empCheck) {
+            case IS_PART_TIME:
+                System.out.println("Employee is Present.");
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                System.out.println("Employee is Present.");
+                empHrs = 8;
+                break;
+            default:
+                System.out.println("Employee is Absent.");
+                empHrs = 0;
+                break;
         }
         empWage = empHrs * EMP_RATE_PER_HR;
         System.out.println("Employee Wage : " + empWage);
